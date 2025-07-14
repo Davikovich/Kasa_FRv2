2423d3c0c0284ecbf22a299915772d2f16e77a0e
Davikovich <davidmendespro@gmail.com>
2025-07-14 12:16:13 +0200

    Mise à jour du router et app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,14 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/index.scss";
+import { BrowserRouter } from "react-router-dom";
+import "./styles/index.scss"; 
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+
